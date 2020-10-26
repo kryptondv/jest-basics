ca0a6fd5667ac6e6ae5bb6fe9611bf290f98c915
kryptondv <kryptondevpl@gmail.com>
2020-10-26 10:42:23 +0100

    add test of asynchronous code

--- functions.js
+++ functions.js
@@ -1,11 +1,18 @@
+const axios = require('axios');
+
 const functions = {
   add: (x, y) => x + y,
   returnNull: () => null,
   checkValue: x => x,
   createUser: (firstName, lastName) => {
-    const user = { firstName, lastName}
+    const user = { firstName, lastName };
     return user;
-  }
+  },
+  fetchUser: () =>
+    axios
+      .get('https://jsonplaceholder.typicode.com/users/1')
+      .then(res => res.data)
+      .catch(err => err)
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -16,10 +16,17 @@ test('should be falsy', () => {
   expect(functions.checkValue(null)).toBeFalsy();
 });
 
-
 test('user should be Bruce Wayne', () => {
   expect(functions.createUser('Bruce', 'Wayne')).toStrictEqual({
     firstName: 'Bruce',
     lastName: 'Wayne',
+  });
+});
+
+// async
+test('user fetched name should be Leanne Graham', () => {
+  expect.assertions(1);
+  return functions.fetchUser().then(data => {
+    expect(data.name).toBe('Leanne Graham');
   });
 });
