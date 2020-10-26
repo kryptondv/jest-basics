ed8c8d7a2e372bcb280cbd09656e0a61ec510f8b
kryptondv <kryptondevpl@gmail.com>
2020-10-26 10:25:49 +0100

    add more functions and tests

--- functions.js
+++ functions.js
@@ -1,5 +1,11 @@
 const functions = {
   add: (x, y) => x + y,
+  returnNull: () => null,
+  checkValue: x => x,
+  createUser: (firstName, lastName) => {
+    const user = { firstName, lastName}
+    return user;
+  }
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,23 @@ const functions = require('./functions');
 test('should output correct sum', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test('should not output incorrect sum', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test('should be null', () => {
+  expect(functions.returnNull()).toBeNull();
+});
+
+test('should be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+
+test('user should be Bruce Wayne', () => {
+  expect(functions.createUser('Bruce', 'Wayne')).toStrictEqual({
+    firstName: 'Bruce',
+    lastName: 'Wayne',
+  });
+});
